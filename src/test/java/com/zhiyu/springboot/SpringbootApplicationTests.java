c92a9bba8e693cfd556cfca71df27e81aee7df20
ZhiyuSun <zcgyxlgy@sina.com>
2020-08-05 17:56:16 +0800

    JDBC

--- src/test/java/com/zhiyu/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/zhiyu/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,33 @@
 package com.zhiyu.springboot;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootApplicationTests {
+public class SpringbootApplicationTests {
+
+    // 注入数据源对象
+    @Autowired
+    private DataSource dataSource;
 
     @Test
-    void contextLoads() {
+    public void datasourceTest() throws SQLException {
+        // 获取数据源类型
+        System.out.println("默认数据源为：" + dataSource.getClass());
+        // 获取数据库连接对象
+        Connection connection = dataSource.getConnection();
+        // 判断连接对象是否为空
+        System.out.println(connection != null);
+        assert connection != null;
+        connection.close();
     }
 
 }
