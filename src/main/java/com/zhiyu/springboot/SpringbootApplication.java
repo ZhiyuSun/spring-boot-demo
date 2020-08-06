8158f7049a5a32112782b72677b4798c1b6c83d8
ZhiyuSun <zcgyxlgy@sina.com>
2020-08-06 18:22:05 +0800

    redis收发消息

--- src/main/java/com/zhiyu/springboot/SpringbootApplication.java
+++ src/main/java/com/zhiyu/springboot/SpringbootApplication.java
@@ -1,8 +1,18 @@
 package com.zhiyu.springboot;
 
+import com.zhiyu.springboot.entity.Receiver;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 @MapperScan("com.zhiyu.springboot.dao")
@@ -12,5 +22,51 @@ public class SpringbootApplication {
         System.out.println("启动 Spring Boot...");
         SpringApplication.run(SpringbootApplication.class, args);
     }
+
+    // redis消息收发，https://spring.io/guides/gs/messaging-redis/
+//    private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootApplication.class);
+//
+//    @Bean
+//    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+//                                            MessageListenerAdapter listenerAdapter) {
+//
+//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+//        container.setConnectionFactory(connectionFactory);
+//        container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+//
+//        return container;
+//    }
+//
+//    @Bean
+//    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+//        return new MessageListenerAdapter(receiver, "receiveMessage");
+//    }
+//
+//    @Bean
+//    Receiver receiver() {
+//        return new Receiver();
+//    }
+//
+//    @Bean
+//    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+//        return new StringRedisTemplate(connectionFactory);
+//    }
+//
+//    public static void main(String[] args) throws InterruptedException {
+//
+//        ApplicationContext ctx = SpringApplication.run(SpringbootApplication.class, args);
+//
+//        StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+//        Receiver receiver = ctx.getBean(Receiver.class);
+//
+//        while (receiver.getCount() < 10) {
+//
+//            LOGGER.info("Sending message...");
+//            template.convertAndSend("chat", "Hello from Redis!");
+//            Thread.sleep(500L);
+//        }
+//
+//        System.exit(0);
+//    }
 
 }
