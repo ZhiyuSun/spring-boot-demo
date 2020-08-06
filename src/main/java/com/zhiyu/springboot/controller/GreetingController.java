b5bcafe96eb9a8510fbe2a5af84416f7b0209f3f
ZhiyuSun <zcgyxlgy@sina.com>
2020-08-06 15:14:35 +0800

    restful

---
+++ src/main/java/com/zhiyu/springboot/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.zhiyu.springboot.controller;
+import com.zhiyu.springboot.entity.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    // https://spring.io/guides/gs/rest-service/
+    // 学习restful service
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
